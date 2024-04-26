4aefdd00101bf501dca83e00ee01f8682628cf66
Guilherme Ziktuel <guilhermeziktuel@gmail.com>
2024-04-26 20:12:03 -0300

    commit

---
+++ app.js
@@ -0,0 +1,37 @@
+
+
+import './database';
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+
+import homeRoutes from './routes/homeRoutes';
+import userRoutes from './routes/userRoutes';
+import tokenRoutes from './routes/tokenRoutes';
+import alunoRoutes from './routes/alunoRoutes';
+
+
+class App {
+  constructor() {
+    this.app = express();
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.app.use(cors(corsOptions));
+    this.app.use(helmet());
+    this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(express.json());
+  }
+
+  routes() {
+    this.app.use('/', homeRoutes);
+    this.app.use('/users/', userRoutes);
+    this.app.use('/tokens/', tokenRoutes);
+    this.app.use('/alunos/', alunoRoutes);
+
+  }
+}
+
+export default new App().app;
---
+++ src/config/database.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+
+module.exports = {
+  
+  dialect: 'sqlite',
+  storage: './db.sqlite',
+
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+    createdAt: 'created_at',
+    updatedAt: 'updated_at',
+  },
+};
